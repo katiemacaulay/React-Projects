352bbfbe3dc857f5737a2c48b9dd693100ff00d9
katiemacaulay <katiemmacaulay@gmail.com>
2018-11-29 18:06:56 -0600

    start of project

---
+++ mylist/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    list: [
+      'cat',
+      'dog',
+      'mouse'
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> My List </h1>
+        <form> 
+          <ul>{this.state.list.map((item, index) => {
+          return(<li key = {index} className = "myList"> {item} </li>)
+          })}
+          </ul>
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
