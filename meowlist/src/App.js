7e6d063423d755d0f26d4021c252c715b88330ff
katiemacaulay <katiemmacaulay@gmail.com>
2018-12-04 22:33:15 -0600

    start of meow notes

--- meowlist/src/App.js
+++ meowlist/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button'
+import sleepycat from './sleepycat.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Button  style={{background: 'blue'}} color="primary"> Make your List Right Meow!</Button>
+          <img src={sleepycat} className="App-logo" alt="logo" />
+          <Button> Make a note before you fall asleep!</Button>
         </header>
       </div>
     );
